0a0d5b4ce07a1e685e3f614400238b9225c683d3
Matheuslemes <matheuslemesmsl@gmail.com>
2025-12-02 17:42:44 -0300

    ecofy(auth): feat - criação de AuthErrorCode.java e AuthException.java.

---
+++ ms-auth/src/main/java/br/com/ecofy/auth/core/application/exception/AuthException.java
@@ -0,0 +1,34 @@
+package br.com.ecofy.auth.core.application.exception;
+
+public class AuthException extends RuntimeException {
+
+    private final AuthErrorCode errorCode;
+    private final String detail;
+
+    public AuthException(AuthErrorCode errorCode, String message) {
+        super(message);
+        this.errorCode = errorCode;
+        this.detail = null;
+    }
+
+    public AuthException(AuthErrorCode errorCode, String message, String detail) {
+        super(message);
+        this.errorCode = errorCode;
+        this.detail = detail;
+    }
+
+    public AuthException(AuthErrorCode errorCode, String message, Throwable cause) {
+        super(message, cause);
+        this.errorCode = errorCode;
+        this.detail = null;
+    }
+
+    public AuthErrorCode getErrorCode() {
+        return errorCode;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+}
